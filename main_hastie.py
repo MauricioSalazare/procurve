502d909706c0dd40a00cf5fadadd7a66c132e2e6
Mauricio Salazar <emsd@kth.se>
2023-06-21 18:59:49 +0200

    Modified project packaging files

--- main_hastie.py
+++ main_hastie.py
@@ -1,6 +1,6 @@
-from src.procurve.principal_curve import PrincipalCurve
-from src.procurve.utils import create_dataset
-from src.procurve.plotting import plot_2d, segments
+from procurve.principal_curve import PrincipalCurve
+from procurve.utils import create_dataset
+from procurve.plotting import plot_2d, segments
 from matplotlib import collections as mc
 import numpy as np
 
@@ -20,6 +20,5 @@ line_collections_fit = segments(pc.last_iteration_log["data_sorted"],
 lc_fit = mc.LineCollection(line_collections_fit, colors="C1", linewidths=0.4, label="Orthogonal projection")
 ax.add_collection(lc_fit)
 ax.legend(fontsize="small")
-
 
 
